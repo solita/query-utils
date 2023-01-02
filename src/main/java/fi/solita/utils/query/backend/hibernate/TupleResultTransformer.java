50f76cdf5cadc6ee41bfdfb41c959247fcf90f5a
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2023-01-02 15:03:35 +0200

    optimization

--- src/main/java/fi/solita/utils/query/backend/hibernate/TupleResultTransformer.java
+++ src/main/java/fi/solita/utils/query/backend/hibernate/TupleResultTransformer.java
@@ -1,17 +1,14 @@
 package fi.solita.utils.query.backend.hibernate;
 
 import static fi.solita.utils.functional.Collections.newArray;
-import static fi.solita.utils.functional.Collections.newMap;
-import static fi.solita.utils.functional.Functional.map;
-import static fi.solita.utils.functional.Functional.zip;
+import static fi.solita.utils.functional.FunctionalA.map;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import org.hibernate.transform.ResultTransformer;
 
 import fi.solita.utils.functional.Pair;
-import fi.solita.utils.functional.SemiGroups;
 import fi.solita.utils.functional.Transformer;
 import fi.solita.utils.functional.Tuple;
 
@@ -30,19 +27,19 @@ public final class TupleResultTransformer implements ResultTransformer {
 
     @Override
     public Object transformTuple(Object[] tuple, String[] aliases) {
-        Map<String, Object> m = newMap(SemiGroups.fail(), zip(aliases, tuple));
+        List<String> aliasList = Arrays.asList(aliases);
         if (aliases.length == 2) {
-            return Pair.of(m.get(a[0]), m.get(a[1]));
+            return Pair.of(tuple[aliasList.indexOf(a[0])], tuple[aliasList.indexOf(a[1])]);
         } else {
-            return Tuple.of(newArray(Object.class, map(getter(m), a)));
+            return Tuple.of(newArray(Object.class, map(getter(aliasList, tuple), a)));
         }
     }
     
-    static final Transformer<String,Object> getter(final Map<String,Object> map) {
+    static final Transformer<String,Object> getter(final List<String> aliasList, final Object[] tuple) {
         return new Transformer<String,Object>() {
             @Override
             public Object transform(String source) {
-                return map.get(source);
+                return tuple[aliasList.indexOf(source)];
             }
         };
     }
