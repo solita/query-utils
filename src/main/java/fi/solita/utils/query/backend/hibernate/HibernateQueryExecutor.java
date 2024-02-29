982e0fadd75a8b94e310fd6da90fa9024ba5afc8
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-02-29 16:14:57 +0200

    hibernate type resolution changed

--- src/main/java/fi/solita/utils/query/backend/hibernate/HibernateQueryExecutor.java
+++ src/main/java/fi/solita/utils/query/backend/hibernate/HibernateQueryExecutor.java
@@ -2,20 +2,24 @@ package fi.solita.utils.query.backend.hibernate;
 
 import static fi.solita.utils.functional.Collections.newArray;
 import static fi.solita.utils.functional.Collections.newList;
+import static fi.solita.utils.functional.Functional.concat;
 import static fi.solita.utils.functional.Functional.head;
 import static fi.solita.utils.functional.Functional.map;
 import static fi.solita.utils.functional.Functional.min;
+import static fi.solita.utils.functional.Option.None;
+import static fi.solita.utils.functional.Option.Some;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-
-import org.hibernate.query.Query;
 import org.hibernate.Session;
 import org.hibernate.proxy.HibernateProxy;
-import org.hibernate.transform.ResultTransformer;
+import org.hibernate.query.BindableType;
+import org.hibernate.query.Query;
+import org.hibernate.query.TupleTransformer;
+import org.hibernate.type.BasicTypeReference;
 
 import fi.solita.utils.functional.ApplyZero;
 import fi.solita.utils.functional.Option;
@@ -152,23 +156,25 @@ public class HibernateQueryExecutor implements JpaCriteriaQueryExecutor, NativeQ
 
     private final org.hibernate.query.NativeQuery<?> bindReturnValues(org.hibernate.query.NativeQuery<?> q, List<Pair<String, Option<Type<?>>>> retvals) {
         for (Entry<String, Option<Type<?>>> param: retvals) {
+            HibernateTypeProvider.HibernateType<?> ht;
             if (param.getValue().isDefined()) {
                 Type<?> type = param.getValue().get();
                 if (type instanceof Type.Optional) {
                     type = ((Type.Optional<?>)type).type;
                 }
-                org.hibernate.type.Type t = ((HibernateTypeProvider.HibernateType<?>)type).type;
-                if (NativeQuery.ENTITY_RETURN_VALUE.equals(param.getKey())) {
-                    q.addEntity(t.getReturnedClass());
-                } else if (t.isEntityType()) {
-                    q.addEntity(param.getKey(), t.getReturnedClass());
-                } else {
-                    q.addScalar(param.getKey(), t);
-                }
+                ht = ((HibernateTypeProvider.HibernateType<?>)type);
             } else {
                 // try to find a type
-                org.hibernate.type.Type t = ((HibernateTypeProvider.HibernateType<?>)typeProvider.type(param.getKey().getClass())).type;
-                q.addScalar(param.getKey(), t);
+                ht = (HibernateTypeProvider.HibernateType<?>)typeProvider.type(param.getKey().getClass());
+            }
+            if (NativeQuery.ENTITY_RETURN_VALUE.equals(param.getKey())) {
+                q.addEntity(head(concat(ht.entity, ht.javaType)));
+            } else if (ht.entity.isDefined()) {
+                q.addEntity(param.getKey(), ht.entity.get());
+            } else if (ht.bindableType.isDefined()) {
+                q.addScalar(param.getKey(), (BasicTypeReference<?>) ht.bindableType.get());
+            } else {
+                q.addScalar(param.getKey(), ht.javaType.get());
             }
         }
         return q;
@@ -194,33 +200,58 @@ public class HibernateQueryExecutor implements JpaCriteriaQueryExecutor, NativeQ
         return q;
     }
 
+    @SuppressWarnings("unchecked")
     private final <T extends Query<?>> T bindParams(T q, Map<String, Pair<?, Option<Type<?>>>> params) {
         for (Entry<String, Pair<?, Option<Type<?>>>> param: params.entrySet()) {
             if (param.getValue()._1 instanceof Collection) {
                 Collection<?> col = (Collection<?>)param.getValue()._1;
+                Option<HibernateTypeProvider.HibernateType<Object>> ht;
                 if (param.getValue()._2.isDefined()) {
-                    q.setParameterList(param.getKey(), (Collection<?>) param.getValue()._1, ((HibernateTypeProvider.HibernateType<?>)param.getValue()._2.get()).type);
+                    ht = Some((HibernateTypeProvider.HibernateType<Object>)param.getValue()._2.get());
                 } else if (!col.isEmpty()) {
                     // try to find a type
-                    org.hibernate.type.Type t = ((HibernateTypeProvider.HibernateType<?>)typeProvider.type(head(col).getClass())).type;
-                    q.setParameterList(param.getKey(), (Collection<?>) param.getValue()._1, t);
+                    ht = Some((HibernateTypeProvider.HibernateType<Object>)typeProvider.type(head(col).getClass()));
                 } else {
                     // fallback to hibernate heuristics when empty collection
+                    ht = None();
                     q.setParameterList(param.getKey(), (Collection<?>) param.getValue()._1);
+
+                }
+                for (HibernateTypeProvider.HibernateType<Object> h: ht) {
+                    for (BindableType<Object> s: h.bindableType) {
+                        q.setParameterList(param.getKey(), (Collection<?>) param.getValue()._1, s);
+                    }
+                    for (Class<?> s: h.javaType) {
+                        q.setParameterList(param.getKey(), (Collection<?>) param.getValue()._1, (Class<Object>)s);
+                    }
+                    for (Class<?> s: h.entity) {
+                        q.setParameterList(param.getKey(), (Collection<?>) param.getValue()._1, (Class<Object>)s);
+                    }
                 }
             } else {
+                HibernateTypeProvider.HibernateType<Object> ht;
+                Object value;
                 if (param.getValue()._2.isDefined()) {
                     Type<?> type = param.getValue()._2.get();
-                    Object value = param.getValue()._1;
+                    value = param.getValue()._1;
                     if (type instanceof Type.Optional) {
                         type = ((Type.Optional<?>) type).type;
                         value = ((Option<?>)value).getOrElse(null);
                     }
-                    q.setParameter(param.getKey(), value, ((HibernateTypeProvider.HibernateType<?>)type).type);
+                    ht = ((HibernateTypeProvider.HibernateType<Object>)type);
                 } else {
                     // try to find a type
-                    org.hibernate.type.Type t = ((HibernateTypeProvider.HibernateType<?>)typeProvider.type(param.getValue()._1.getClass())).type;
-                    q.setParameter(param.getKey(), param.getValue()._1, t);
+                    value = param.getValue()._1;
+                    ht = (HibernateTypeProvider.HibernateType<Object>)typeProvider.type(param.getValue()._1.getClass());
+                }
+                for (BindableType<Object> s: ht.bindableType) {
+                    q.setParameter(param.getKey(), value, s);
+                }
+                for (Class<?> s: ht.javaType) {
+                    q.setParameter(param.getKey(), value, (Class<Object>)s);
+                }
+                for (Class<?> s: ht.entity) {
+                    q.setParameter(param.getKey(), value, (Class<Object>)s);
                 }
             }
         }
--- src/main/java/fi/solita/utils/query/backend/hibernate/HibernateTypeProvider.java
+++ src/main/java/fi/solita/utils/query/backend/hibernate/HibernateTypeProvider.java
@@ -1,35 +1,23 @@
 package fi.solita.utils.query.backend.hibernate;
 
-import static fi.solita.utils.functional.Collections.newList;
-import static fi.solita.utils.functional.Collections.newMap;
-import static fi.solita.utils.functional.Functional.filter;
-import static fi.solita.utils.functional.Functional.find;
-import static fi.solita.utils.functional.Functional.flatMap;
-import static fi.solita.utils.functional.Functional.groupBy;
-import static fi.solita.utils.functional.Functional.map;
 import static fi.solita.utils.functional.Option.None;
 import static fi.solita.utils.functional.Option.Some;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Map;
 
-
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
-import org.hibernate.TypeHelper;
-import org.hibernate.metadata.ClassMetadata;
-import org.hibernate.metamodel.spi.MetamodelImplementor;
-import org.hibernate.persister.entity.EntityPersister;
-import org.hibernate.proxy.HibernateProxyHelper;
-import org.hibernate.usertype.CompositeUserType;
-import org.hibernate.usertype.UserType;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.metamodel.MappingMetamodel;
+import org.hibernate.query.BindableType;
+import org.hibernate.type.BasicTypeReference;
+import org.hibernate.type.descriptor.java.JavaType;
+import org.hibernate.type.spi.TypeConfiguration;
 
 import fi.solita.utils.functional.ApplyZero;
 import fi.solita.utils.functional.Functional;
 import fi.solita.utils.functional.Option;
-import fi.solita.utils.functional.Pair;
-import fi.solita.utils.functional.Predicates;
-import fi.solita.utils.functional.SemiGroups;
 import fi.solita.utils.functional.Transformer;
 import fi.solita.utils.query.IEntity;
 import fi.solita.utils.query.Identifiable;
@@ -40,10 +28,27 @@ import jakarta.persistence.EntityManager;
 public class HibernateTypeProvider implements TypeProvider {
 
     public static class HibernateType<T> implements Type<T> {
-        public final org.hibernate.type.Type type;
+        public final Option<BindableType<T>> bindableType;
+        public final Option<Class<?>> entity;
+        public final Option<Class<?>> javaType;
+        
 
-        public HibernateType(org.hibernate.type.Type type) {
-            this.type = type;
+        private HibernateType(Option<BindableType<T>> bindableType, Option<Class<?>> entity, Option<Class<?>> javaType) {
+            this.bindableType = bindableType;
+            this.entity = entity;
+            this.javaType = javaType;
+        }
+        
+        public static <T> Type<T> bindable(BindableType<T> x) {
+            return new HibernateType<T>(Some(x), Option.<Class<?>>None(), Option.<Class<?>>None());
+        }
+        
+        public static <T> Type<T> entity(Class<T> x) {
+            return new HibernateType<T>(Option.<BindableType<T>>None(), Option.<Class<?>>Some(x), Option.<Class<?>>None());
+        }
+        
+        public static <T> Type<T> javaType(Class<T> x) {
+            return new HibernateType<T>(Option.<BindableType<T>>None(), Option.<Class<?>>None(), Option.<Class<?>>Some(x));
         }
     }
 
@@ -63,111 +68,43 @@ public class HibernateTypeProvider implements TypeProvider {
         }
     };
     
-    // cache these, since it's static data.
-    private Map<Class<?>,String> typesByUniqueReturnedClassCache;
-    private Map<Class<?>,String> typesByUniqueReturnedClass() {
-        if (typesByUniqueReturnedClassCache == null) {
-            MetamodelImplementor                       metamodelImpl              = (MetamodelImplementor) em.get().unwrap(Session.class).getSessionFactory().getMetamodel();
-            Iterable<ClassMetadata>                    allClassMetadata           = map(HibernateTypeProvider_.entityPersister2ClassMetadata, metamodelImpl.entityPersisters().values());
-            Map<String, List<org.hibernate.type.Type>> allPropertyTypesByName     = groupBy(HibernateTypeProvider_.type2Name, flatMap(HibernateTypeProvider_.classMetadata2propertyTypes, allClassMetadata));
-            Iterable<org.hibernate.type.Type>          allDifferentPropertyTypes  = map(head, allPropertyTypesByName.values());
-            Iterable<List<org.hibernate.type.Type>>    typesByReturnedClass       = groupBy(HibernateTypeProvider_.type2ReturnedClass, allDifferentPropertyTypes).values();
-            Iterable<List<org.hibernate.type.Type>>    typesUniqueByReturnedClass = filter(size.andThen(Predicates.equalTo(1l)), typesByReturnedClass);
-            typesByUniqueReturnedClassCache = newMap(SemiGroups.<String>fail(), map(head.andThen(HibernateTypeProvider_.type2ReturnedClassAndNamePair), typesUniqueByReturnedClass));
-        }
-        return typesByUniqueReturnedClassCache;
-    }
-    
     public HibernateTypeProvider(ApplyZero<EntityManager> em) {
         this.em = em;
     }
-
-    static ClassMetadata entityPersister2ClassMetadata(EntityPersister ep) {
-        return (ClassMetadata) ep;
-    }
-
-    static List<org.hibernate.type.Type> classMetadata2propertyTypes(ClassMetadata c) {
-        return newList(c.getPropertyTypes());
-    }
-    
-    static Class<?> type2ReturnedClass(org.hibernate.type.Type type) {
-        return type.getReturnedClass();
-    }
-    
-    static String type2Name(org.hibernate.type.Type type) {
-        return type.getName();
-    }
     
     @SuppressWarnings("unchecked")
-    static Pair<Class<?>,String> type2ReturnedClassAndNamePair(org.hibernate.type.Type type) {
-        return (Pair<Class<?>,String>)(Object)Pair.of(type.getReturnedClass(), type.getName());
-    }
-
     @Override
     public <ID extends Serializable, T extends Identifiable<ID>> Type<ID> idType(Class<T> entityType) {
-        return new HibernateType<ID>(((MetamodelImplementor) em.get().unwrap(Session.class).getSessionFactory().getMetamodel()).entityPersister(entityType).getIdentifierType());
-    }
-    
-    @SuppressWarnings("unchecked")
-    private static <T> Option<Class<T>> getEnumType(Class<T> type) {
-        if (type.isEnum()) {
-            return Some((Class<T>) type);
-        }
-        if (type.getEnclosingClass() != null && type.getEnclosingClass().isEnum()) {
-            return Some((Class<T>) type.getEnclosingClass());
-        }
-        return None();
+        SessionFactoryImplementor sf = (SessionFactoryImplementor) em.get().unwrap(Session.class).getSessionFactory();
+        org.hibernate.type.Type t = ((MappingMetamodel) sf.getMappingMetamodel()).getEntityDescriptor(entityType).getIdentifierType();
+        return HibernateType.javaType((Class<ID>)t.getReturnedClass());
     }
 
     @Override
     public <T> Type<T> type(final Class<T> clazz) {
-        TypeHelper typeHelper = em.get().unwrap(Session.class).getTypeHelper();
+        TypeConfiguration typeHelper = em.get().getEntityManagerFactory().unwrap( SessionFactoryImplementor.class ).getTypeConfiguration();
         
         // basic type?
-        org.hibernate.type.Type basicType = typeHelper.basic(clazz);
+        org.hibernate.type.BasicType<T> basicType = typeHelper.getBasicTypeForJavaType(clazz);
         if (basicType != null) {
-            return new HibernateType<T>(basicType);
-        }
-        
-        // user type?
-        if (UserType.class.isAssignableFrom(clazz) || CompositeUserType.class.isAssignableFrom(clazz)) {
-            org.hibernate.type.Type customType = typeHelper.custom(clazz);
-            if (customType != null) {
-                return new HibernateType<T>(customType);
-            }
+            return HibernateType.bindable(new BasicTypeReference<T>(basicType.getName(), clazz, basicType.getJdbcType().getDefaultSqlTypeCode()));
         }
         
         // entity?
         try {
             em.get().getMetamodel().entity(clazz);
-            org.hibernate.type.Type entityType = typeHelper.entity(clazz);
+            JavaType<T> entityType = typeHelper.getJavaTypeRegistry().resolveEntityTypeDescriptor(clazz);
             if (entityType != null) {
-                return new HibernateType<T>(entityType);
+                return HibernateType.entity(clazz);
             }
         } catch (IllegalArgumentException e) {
             // was not an entity...
         }
         
-        // no luck with standard stuff, try searching a unique match from all entity metadata
-        for (String typeName: find(clazz, typesByUniqueReturnedClass())) {
-            org.hibernate.type.Type heuristicType = typeHelper.heuristicType(typeName);
-            if (heuristicType != null) {
-                return new HibernateType<T>(heuristicType);
-            }
-        }
-        
-        // enum type?
-        for (Class<?> enumClass: getEnumType(clazz)) {
-            org.hibernate.type.Type heuristicType = typeHelper.heuristicType(enumClass.getName());
-            if (heuristicType != null) {
-                return new HibernateType<T>(heuristicType);
-            }
-        }
-        
-        // still no luck. Fallback to Hibernate heuristics
-        org.hibernate.type.Type heuristicType = typeHelper.heuristicType(clazz.getName());
-        if (heuristicType != null) {
-            return new HibernateType<T>(heuristicType);
+        // custom type?
+        JavaType<Object> customType = typeHelper.getJavaTypeRegistry().resolveDescriptor(clazz);
+        if (customType != null) {
+            return HibernateType.javaType(clazz);
         }
         
         throw new IllegalArgumentException("Could not resolve Hibernate Type for: " + clazz + ". Please provide the type explicitly");
@@ -175,6 +112,6 @@ public class HibernateTypeProvider implements TypeProvider {
 
     @Override
     public Class<?> getEntityClass(IEntity<?> entity) {
-        return HibernateProxyHelper.getClassWithoutInitializingProxy(entity);
+        return Hibernate.getClassLazy(entity);
     }
 }
